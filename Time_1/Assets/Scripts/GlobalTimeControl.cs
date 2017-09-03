af937baf969f3c85b11d6afde49af82994904949
Kayabaakiko <464186281>
2017-09-03 16:37:44 +0800

    1

---
+++ Time_1/Assets/Scripts/GlobalTimeControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalTimeControl : MonoBehaviour {
+
+
+	public Transform reference;
+	public float timeSpeed;
+	//public TimeRewind scritp;
+	public 
+
+
+
+	// Use this for initialization
+	void Start () {
+		//scritp = reference.GetComponent<TimeRewind>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+
+		
+	}
+}
